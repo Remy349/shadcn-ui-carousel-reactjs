8b425a0f88e5a582063f05ec036118c092756fa9
Remy349 <santoraga15@gmail.com>
2025-01-20 13:07:57 -0600

    feat: Add custom carousel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { CustomCarousel } from "./custom-carousel";
+
 export const App = () => {
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <h1 className="font-bold text-2xl">APP FILE</h1>
-    </div>
+    <section className="pt-[4rem] pb-[2.5rem]">
+      <div className="mx-auto px-6 lg:max-w-5xl xl:max-w-7xl">
+        <h1 className="font-bold text-2xl text-center mb-8 md:text-3xl">
+          Shadcn UI Carousel built with ReactJS
+        </h1>
+        <CustomCarousel />
+      </div>
+    </section>
   );
 };
